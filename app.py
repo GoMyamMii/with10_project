56020cac188ec37e9941b5caae4fdafcb5cdc61e
GoMyamMii <113406893+GoMyamMii@users.noreply.github.com>
2022-11-02 11:11:33 +0900

    DB 확인용
    어디가 문제일까요 ,,

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template, request, jsonify
+app = Flask(__name__)
+
+from pymongo import MongoClient
+client = MongoClient('mongodb+srv://test:sparta@cluster0.tlxtldo.mongodb.net/Cluster0?retryWrites=true&w=majority')
+db = client.dbsparta
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route("/comments", methods=["POST"])
+def comment_post():
+    name_receive = request.form['name_give']
+    comment_receive = request.form['comment_give']
+
+    comment_list = list(db.comments.find({}, {'_id': False}))
+    count = len(comment_list) + 1
+
+    doc = {
+        'name':name_receive,
+        'comment':comment_receive,
+        'numb':count,
+        'good':0
+    }
+    db.comments.insert_one(doc)
+
+    return jsonify({'msg': '등록 완료!'})
+
+
+@app.route("/comments", methods=["GET"])
+def comment_get():
+    comment_list = list(db.comments.find({}, {'_id': False}))
+
+    return jsonify({'comments':comment_list})
+
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
