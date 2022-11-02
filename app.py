5f8ddcacdb4f9cb1539e9494ceda3cb53577a412
kimnamgyu93 <117063182+Kimnamgyu93@users.noreply.github.com>
2022-11-02 14:37:52 +0900

    Add files via upload
    댓글 추가/삭제

--- app.py
+++ app.py
@@ -2,32 +2,12 @@ from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
 
 from pymongo import MongoClient
-client = MongoClient('mongodb+srv://test:sparta@cluster0.tlxtldo.mongodb.net/Cluster0?retryWrites=true&w=majority')
+client = MongoClient('mongodb+srv://test:sparta@cluster0.e52hpkd.mongodb.net/Cluster0?retryWrites=true&w=majority')
 db = client.dbsparta
 
 @app.route('/')
 def home():
     return render_template('index.html')
-
-@app.route('/cs')
-def cs():
-    return render_template('cs.html')
-
-@app.route('/hn')
-def hn():
-    return render_template('hanul.html')
-
-@app.route('/hh')
-def hh():
-    return render_template('heehyeon.html')
-
-@app.route('/js')
-def js():
-    return render_template('JS.html')
-
-@app.route('/ng')
-def ng():
-    return render_template('kng.html')
 
 
 @app.route("/comments", methods=["POST"])
@@ -39,21 +19,27 @@ def comment_post():
     count = len(comment_list) + 1
 
     doc = {
+        'num':count,
         'name':name_receive,
         'comment':comment_receive,
-        'numb':count,
-        'good':0
+        'done':0
     }
+
     db.comments.insert_one(doc)
 
-    return jsonify({'msg': '등록 완료!'})
+    return jsonify({'msg': '작성 완료!'})
 
+@app.route("/comments/done", methods=["POST"])
+def comment_done():
+    num_receive = request.form['num_give']
+    db.comments.delete_one({'num': int(num_receive)})
+    return jsonify({'msg': '삭제 완료!'})
 
-@app.route("/comment", methods = ["GET"])
-def comment_get() :
-    comment_list = list(db.comment.find({}, {'_id': False}))
-    return jsonify({'comments' : comment_list})
+@app.route("/comments", methods=["GET"])
+def comment_get():
+    comment_list = list(db.comments.find({}, {'_id': False}))
 
+    return jsonify({'cmts':comment_list})
 
 
 if __name__ == '__main__':
