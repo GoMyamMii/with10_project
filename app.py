2d6c2e9bdae4d591e1baf979c26d897c4bb72f09
hiheecho <jhh7929@gmail.com>
2022-11-02 15:18:43 +0900

    index 수정 및 app.py 취합

--- app.py
+++ app.py
@@ -2,12 +2,32 @@ from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
 
 from pymongo import MongoClient
-client = MongoClient('mongodb+srv://test:sparta@cluster0.e52hpkd.mongodb.net/Cluster0?retryWrites=true&w=majority')
+client = MongoClient('mongodb+srv://test:sparta@cluster0.tlxtldo.mongodb.net/Cluster0?retryWrites=true&w=majority')
 db = client.dbsparta
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/cs')
+def cs():
+    return render_template('cs.html')
+
+@app.route('/hn')
+def hn():
+    return render_template('hanul.html')
+
+@app.route('/hh')
+def hh():
+    return render_template('heehyeon.html')
+
+@app.route('/js')
+def js():
+    return render_template('JS.html')
+
+@app.route('/ng')
+def ng():
+    return render_template('kng.html')
 
 
 @app.route("/comments", methods=["POST"])
@@ -19,28 +39,28 @@ def comment_post():
     count = len(comment_list) + 1
 
     doc = {
-        'num':count,
         'name':name_receive,
         'comment':comment_receive,
-        'done':0
+        'numb':count,
+        'good':0
     }
-
     db.comments.insert_one(doc)
 
-    return jsonify({'msg': '작성 완료!'})
+    return jsonify({'msg': '등록 완료!'})
 
-@app.route("/comments/done", methods=["POST"])
-def comment_done():
+
+
+@app.route("/comments/delete", methods=["POST"])
+def comment_delete():
     num_receive = request.form['num_give']
     db.comments.delete_one({'num': int(num_receive)})
     return jsonify({'msg': '삭제 완료!'})
 
-@app.route("/comments", methods=["GET"])
+
+@app.route("/comments", methods = ["GET"])
 def comment_get():
     comment_list = list(db.comments.find({}, {'_id': False}))
-
-    return jsonify({'cmts':comment_list})
-
+    return jsonify({'comments': comment_list})
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=5000, debug=True)
+    app.run('0.0.0.0', port=3000, debug=True)
